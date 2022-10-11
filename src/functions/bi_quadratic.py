Tune hyperarameters and fix hessian bug in f3

src/functions/bi_quadratic.py
@@ -21,15 +21,15 @@ class BiQuadraticFunction:
     def hessian(x):
         x = x.ravel()
         hessian = np.array([
-            [- 400 * (x[1] - 3 * x[0] ** 2) * 2 * x[0] + 2, - 400 * x[0]],
+            [- 400 * (x[1] - 3 * x[0] ** 2) + 2, - 400 * x[0]],
             [-400 * x[0], 200]
         ])
         return hessian
 
     @staticmethod
-    def initialize(x0=None, n=None):
+    def initialize(x0=None, n=2):
         if x0:
             x0 = np.array(x0).reshape(-1, 1)
         else:
-            x0 = np.random.randint(2, 5, (n, 1))
+            x0 = np.random.randint(2, 20, (n, 1)) + .1
         return x0

src/functions/quadratic.py
@@ -25,7 +25,7 @@ class QuadraticFunction:
         if x0:
             x0 = np.array(x0).reshape(-1, 1)
         else:
-            x0 = np.random.randint(2, 5, (n, 1))
+            x0 = np.random.randint(1, 333, (n, 1)) + .1
         return x0
 
 

src/optimizers/newton_method.py
@@ -12,10 +12,14 @@ def newton_method(f, x0, hessian, alpha=1e-4, max_iters=1000,
     if not grad:
         grad = lambda inp: grad_f_estimate(f, inp)
 
+    fx_ls = []
+
     for k in range(max_iters):
         grad_k = grad(x_k)
         hessian_k = hessian(x_k)
         p_k = - np.linalg.inv(hessian_k) @ grad_k
+
+        fx_ls.append(f(x_k))
 
         if display:
             print(f"x_k: {x_k}, \nfunction: {f(x_k)}, \ngrad: {grad_k}\n")
@@ -28,4 +32,4 @@ def newton_method(f, x0, hessian, alpha=1e-4, max_iters=1000,
 
         x_k = x_k_next
 
-    return k, x_k, f(x_k)
+    return k, x_k, f(x_k), fx_ls

src/optimizers/quasi_newton_method.py
@@ -14,10 +14,11 @@ def quasi_newton_method(f, x0, alpha=1e-4, max_iters=1000,
 
     n, _ = x_k.shape
     F_k = np.eye(n)
-
+    fx_ls = []
     for k in range(max_iters):
         grad_k = grad(x_k)
         p_k = - F_k @ grad_k
+        fx_ls.append(f(x_k))
 
         if display:
             print(f"x_k: {x_k}, \nfunction: {f(x_k)}, \ngrad: {grad_k}\n")
@@ -36,4 +37,4 @@ def quasi_newton_method(f, x0, alpha=1e-4, max_iters=1000,
 
         x_k = x_k_next
 
-    return k, x_k, f(x_k)
+    return k, x_k, f(x_k), fx_ls

src/run.py
@@ -1,16 +1,25 @@
+import numpy as np
+
 from .func_opt_factory import get_function, get_optimizer
-from .utils import process_config, dump_result, read_config
+from .utils import process_config, dump_result, read_config, visualize
+
+SEED = 1476585
 
 if __name__ == '__main__':
     cfg = read_config()
     for func_, opt_config in cfg.items():
+        fx_ls = {}
         for opt_, config in opt_config.items():
+            np.random.seed(SEED)
+
             opt_func = get_function(func_)()
 
             print(func_, opt_, config)
             config = process_config(opt_func, config)
 
             optimizer = get_optimizer(opt_)
-            result = optimizer(**config)
-            # print(result, "\n")
+            *result, fx_ls_ = optimizer(**config)
             dump_result(func_, opt_, result)
+
+            fx_ls[opt_] = fx_ls_
+        visualize(fx_ls, func_)